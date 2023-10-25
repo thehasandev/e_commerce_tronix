994a279548e30b238d3776993ca028c0570dcf6b
Hasan <thehasandev@gmail.com>
2023-10-25 18:29:36 +0600

    image-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Container from './components/Container'
 import Flex from './components/Flex'
+import Image from './components/Image'
+import Test from "./assets/test.jpg"
 
 function App() {
   return (
@@ -8,8 +10,7 @@ function App() {
       <Container>
         <Flex className="gap-x-10">
           <h1 className='font-pop text-primary '>This is My Tailwind Setup</h1>
-          <h1 className='font-mon text-secondary '>This is My Tailwind Setup</h1>
-          <h1 className='font-nunito text-primary '>This is My Tailwind Setup</h1>
+          <Image src={Test} alt="Test" className="w-32"/>
         </Flex>
       </Container>
     </div>
