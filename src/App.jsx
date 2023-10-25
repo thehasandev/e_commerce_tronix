75c747fe245ed1ed973bc8ac00e624cc51918c30
Hasan <thehasandev@gmail.com>
2023-10-25 18:23:34 +0600

    felx-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Container from './components/Container'
+import Flex from './components/Flex'
 
 function App() {
   return (
     <div>
       <Container>
-        <h1 className='font-pop text-primary '>This is My Tailwind Setup</h1>
-        <h1 className='font-mon text-secondary '>This is My Tailwind Setup</h1>
-        <h1 className='font-nunito text-primary '>This is My Tailwind Setup</h1>
+        <Flex className="gap-x-10">
+          <h1 className='font-pop text-primary '>This is My Tailwind Setup</h1>
+          <h1 className='font-mon text-secondary '>This is My Tailwind Setup</h1>
+          <h1 className='font-nunito text-primary '>This is My Tailwind Setup</h1>
+        </Flex>
       </Container>
     </div>
   )
