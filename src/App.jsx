ea0ebdbd447373acad49dda54f15d90f88f772ea
Hasan <thehasandev@gmail.com>
2023-10-25 18:31:47 +0600

    section-component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Container from './components/Container'
 import Flex from './components/Flex'
 import Image from './components/Image'
 import Test from "./assets/test.jpg"
+import Section from './components/Section'
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
           <h1 className='font-pop text-primary '>This is My Tailwind Setup</h1>
           <Image src={Test} alt="Test" className="w-32"/>
         </Flex>
+        <Section>
+          This is My Section Components
+        </Section>
       </Container>
     </div>
   )
