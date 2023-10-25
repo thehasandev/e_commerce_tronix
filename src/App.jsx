93e0d733a680b0b970cd8bd2c5161b0c77894b50
Hasan <thehasandev@gmail.com>
2023-10-25 18:53:14 +0600

    top-conract-component

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import Image from './components/Image'
 
 import Section from './components/Section'
 import List from './components/List'
+import Contact from './components/Contact'
+import {AiFillPhone} from "react-icons/ai"
 
 function App() {
   return (
     <div>
       <Container>
-        <ul>
-          <List text="Home"/>
-          <List text="Home"/>
-          <List text="About"/>
-          <List text="Service"/>
+        <ul className='flex gap-x-5 items-center'>
+          <Contact text="+12 345 6789 0" icon={<AiFillPhone/>}/>
+          <Contact text="+12 345 6789 0" icon={<AiFillPhone/>}/>
+          <Contact text="+12 345 6789 0" icon={<AiFillPhone/>}/>
         </ul>
       </Container>
     </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Flex from './Flex'
 
-function Contact() {
+
+function Contact({text,icon}) {
   return (
-    <div>
-
-    </div>
+    <Flex className="gap-x-4 items-center">
+        <div className='text-primary'> 
+           {icon}
+        </div>
+        <div>
+           <p className='font-pop font-normal text-small text-secondary'>{text}</p>
+        </div>
+    </Flex>
   )
 }
 
