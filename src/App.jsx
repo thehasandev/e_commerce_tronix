65589e431b90bb9a942d5d135fb3bea69d1dceeb
Hasan <thehasandev@gmail.com>
2023-10-25 18:42:33 +0600

    list-component

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Container from './components/Container'
 import Flex from './components/Flex'
 import Image from './components/Image'
-import Test from "./assets/test.jpg"
+
 import Section from './components/Section'
+import List from './components/List'
 
 function App() {
   return (
     <div>
       <Container>
-        <Flex className="gap-x-10">
-          <h1 className='font-pop text-primary '>This is My Tailwind Setup</h1>
-          <Image src={Test} alt="Test" className="w-32"/>
-        </Flex>
-        <Section>
-          This is My Section Components
-        </Section>
+        <ul>
+          <List text="Home"/>
+          <List text="Home"/>
+          <List text="About"/>
+          <List text="Service"/>
+        </ul>
       </Container>
     </div>
   )
---
+++ src/components/List.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function List({text}) {
+  return (
+    <li className='font-pop font-normal text-secondary text-small'>{text}</li>
+  )
+}
+
+export default List
