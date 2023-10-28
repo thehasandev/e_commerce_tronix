ec92592ac9328f8613f32587df422ef9874f2d91
Hasan <thehasandev@gmail.com>
2023-10-28 18:00:00 +0600

    arrival-data

---
+++ src/Data/arrival.js
@@ -0,0 +1,23 @@
+let arrival =[
+    {
+        url : "/images/cetagori1.png",
+        name : "Smartphone 5G",
+        price : 500,
+        badge : "new"
+    },
+    {
+        url : "/images/cetagori1.png",
+        name : "USB Speaker Portable",
+        price : 44,
+        badge : "new"
+    },
+    {
+        url : "/images/cetagori1.png",
+        name : "Smart TV",
+        price : 600,
+        badge : "new"
+    },
+
+]
+
+export default arrival
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,8 @@ import "slick-carousel/slick/slick.css";
 import Slider from "react-slick";
 import SubHeading from "../components/SubHeading"
 import Categori from "../components/Categori"
+import Arrival from "../components/Arrival"
+
 function Home() {
     const bannerSlide = {
         dots: true,
@@ -31,6 +33,7 @@ function Home() {
             </div>
           )
       };
+
   return (
     <>
        <Section>
@@ -54,22 +57,29 @@ function Home() {
         </Container>
        </Section>
 
-
        <Section className="my-20">
           <Container>
              <SubHeading text="Category" className="mb-10"/>
              <Flex className="justify-between">
                 {
                     cetagorData.map((item,index)=>(
-
                         <Categori key={index} src={item.url} text={item.name}/>
-
                     ))
                 }
              </Flex>
               
           </Container>
        </Section>
+       
+       <Section>
+         <Container>
+           <SubHeading text="New Arrival"/>
+           <Flex>
+             <Arrival/>
+           </Flex>
+         </Container>
+       </Section>
+
     </>
   )
 }
