10ae22a83a0b376732068d6b638a1bac9aba75d2
Hasan <thehasandev@gmail.com>
2023-10-28 22:19:12 +0600

    team-part

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,6 +10,8 @@ import ChooseOne from "../assets/choosing1.png"
 import ChooseTwo from "../assets/choosing2.png"
 import ChooseThree from "../assets/choosing3.png"
 import ChooseFour from "../assets/choosing4.png"
+import Team from "../components/Team"
+import TeamOne from "../assets/team1.png"
 
 function About() {
   return (
@@ -31,6 +33,18 @@ function About() {
                 <Choosing src={ChooseTwo} name="Fast Delivery" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do. "/>
                 <Choosing src={ChooseThree} name="COD Service" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do. "/>
                 <Choosing src={ChooseFour} name="100% Original Products" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do. "/>
+            </Flex>
+        </Container>
+     </Section>
+
+     <Section className="mt-40">
+        <Container>
+            <SubHeading text="Our Team" className="text-center text-5xl mb-20"/>
+            <Flex className="justify-between flex-wrap">
+               <Team src={TeamOne} name="Sam Alabama" title="Team Member"/>
+               <Team src={TeamOne} name="Ahmad Pucelle" title="Team Member"/>
+               <Team src={TeamOne} name="Jack Lentera" title="Team Member"/>
+               <Team src={TeamOne} name="Ahmad Pucelle" title="Team Member"/>
             </Flex>
         </Container>
      </Section>
