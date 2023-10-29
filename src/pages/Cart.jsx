fd46fdfa8e34012a806aba1939504a1f7ce5e757
Hasan <thehasandev@gmail.com>
2023-10-29 23:36:55 +0600

    cart-part-complete

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Section from '../components/Section'
 import Container from '../components/Container'
-import {AiOutlineCloseSquare} from "react-icons/ai"
+import {AiOutlineClose,AiOutlineRight} from "react-icons/ai"
 import { useDispatch, useSelector } from 'react-redux'
 import { increment,decrement,removeCart } from '../slices/cartSlice'
 import Flex from '../components/Flex'
+import Image from "../components/Image"
+import Voucer from "../assets/voucer.png"
 
 function Cart() {
+    let datas = useSelector((state)=>state.brade.previousPage)
     let [total,setTotal] =useState(0)
     let data = useSelector((state)=>state.cart.cartItem)
     let dispatch = useDispatch()
@@ -27,72 +30,82 @@ function Cart() {
         })
         setTotal(tottal)
      },[data])
+
+
+
   return (
     <>
+       <Section className="mb-8">
+            <Container>
+                <Flex className="items-center">
+                    <p className='capitalize font-pop font-normal text-small text-gray mr-2'>{datas}</p>
+                    <AiOutlineRight className='text-gray'/>
+                    <p className='capitalize font-pop font-normal text-small text-primary ml-2'>{(window.location.pathname.replace("/",""))}</p>
+                </Flex>
+            </Container>
+        </Section>
+
      <Section>
         <Container>
-            <ul className='bg-ash flex justify-between p-4 relative'>
-                <li className='font-dm font-bold text-base text-primary'>Action</li>
-                <li className='font-dm font-bold text-base text-primary'>Product</li>
-                <li className='font-dm font-bold text-base text-primary'>Name</li>
-                <li className='font-dm font-bold text-base pl-10 text-primary'>Price</li>
-                <li className='font-dm font-bold text-base text-primary'>Quantity</li>
-                <li className='font-dm font-bold  text-base text-primary'>Sub Total</li> 
-            </ul>
+          
+            <Flex className="mb-20 justify-between">
+                      
+                         <div className='w-7/12'>
+                            {
+                                data.map((item,index)=>(
+                                    <div className='w-full mb-8'>
+                                            <Flex className='w-full gap-x-14 border-b border-solid border-primary pb-8 items-center'>  
+                                             
+                                            <div className='w-96'>
+                                                <Image src={item.url}/>
+                                            </div>
+                                                
+                                            <div className='w-full'>
+                                                <h3 className='font-dm font-bold text-2xl text-secondary'>USB Speaker Portable</h3>
+                                                <Flex className="flex justify-between items-center mb-4 mt-12">
+                                                <p className='font-dm font-bold text-2xl text-primary  '>{`$ ${item.price*item.quantity} : 00`} </p>
+                                                <AiOutlineClose onClick={()=>{handleRemove(item)}}  className='cursor-pointer' size={25}/>
+                                                </Flex>
+                                            
+                                            <Flex className="gap-x-8 items-center">
+                                                    <button onClick={()=>{handleDecrement(item)}} className='px-5 py-3 rounded-lg bg-gray text-secondary text-3xl font-semibold hover:bg-primary hover:text-white duration-200'>-</button>
 
-          {
-            data.map((item,index)=>{
-        return <ul key={index} className='bg-ash flex items-center  p-4 border-b border-gray'>
-                  <li className='w-[280px] font-dm font-bold text-base text-secondary cursor-pointer' onClick={()=>{handleRemove(item)}} >
-                  <AiOutlineCloseSquare onClick={()=>{handleRemove(item)}} size={30} className='text-secondary'/>
-                  </li>
-                  <li className='  font-dm font-bold text-base text-secondary'><img src={item.url} alt="img1" className='w-16'/></li>
-                  <li className='w-[550px] text-center  font-dm font-bold text-base text-secondary'>{item.name}</li>
-                  <li className='w-[200px] pl-8  font-dm font-bold text-base  text-secondary'>1000</li>
-                  <li className=' flex  items-center px-1 justify-center gap-5 font-dm border border-secondary border-solid ml-24 font-bold text-base text-secondary'>
-                      <button onClick={()=>{handleDecrement(item)}}  className='text-lg text-secondary'>-</button>
-                               {item.quantity}
-                      <button  onClick={()=>{handleIncrement(item)}} className='text-lg  text-secondary'>+</button>
-                  </li>
-                  <li className='w-[300px] text-right   font-dm font-bold  text-base text-secondary'>{item.price*item.quantity}</li> 
-               </ul>
-                
-            })
-          }
-           
+                                                    <p className='font-dm font-bold text-2xl text-primary'>{item.quantity}</p>
+                                                    <button onClick={()=>{handleIncrement(item)}}  className='px-5 py-3 rounded-lg bg-gray text-secondary text-3xl font-semibold hover:bg-primary hover:text-white duration-200'>+</button>
+                                            </Flex>
+                                            </div>
+
+                                            </Flex>
+
+                                    </div>
+
+                                ))
+                            }                 
+                         </div>
+
+            
+                   
+                <div className='w-4/12'>
+                    <div className='border border-primary rounded-lg border-solid px-16 py-8 flex items-center justify-between'>
+                        <Image src={Voucer}/>
+                        <p className='font-pop font-medium text-2xl text-secondary'>Have a coupon code?</p>
+                        <AiOutlineRight/>
+                    </div>
+                    <div className='border rounded-lg border-primary border-solid px-16 py-8 mt-8 '>
+                        <p className='font-pop font-medium text-2xl text-secondary'>Summary</p>
+                            <Flex className="justify-between mb-12 mt-8">
+                                <p className='font-pop font-medium text-small text-gray'>Total</p>
+                                <p className='font-pop font-medium text-small text-primary'>${total}.00</p>
+                            </Flex>
+                        <button className='w-full block rounded-lg  py-4 bg-primary font-pop text-lg text-white font-medium'>Checkout</button>
+                        <p className='font-pop font-medium text-small text-primary mt-8 text-center'>Continue Shopping</p>
+                    </div>
+                </div>
+
+            </Flex>  
         </Container>
      </Section>
 
-     <section>
-        <Container>
-            <Flex className="justify-end mt-10">
-              <table>
-                      <thead>
-                          <tr>
-                              <th className='border border-gray/50  font-dm font-bold text-base text-primary py-4 px-32'>Subtotal</th>
-                              <th className='border border-gray/50 font-dm font-normal text-base text-gray py-4 px-32'>{total}</th>  
-                          </tr>
-                      </thead>
-
-                      <tbody>
-                          <tr>
-                              <td className='border border-gray/50 font-dm font-bold text-base text-primary py-4 px-32'>Total</td>
-                              <td className='border border-gray/50 font-dm font-bold text-base text-secondary py-4 px-32'>{total}</td>  
-                          </tr>
-                      </tbody>
-                      
-              </table>
-            </Flex> 
-
-            <Flex className="justify-end mt-5">
-                {/* <Link to="/checkout"> */}
-                <button className='font-pop font-medium text-lg bg-secondary border border-solid border-secondary hover:bg-transparent px-8 py-3 rounded-[5px] text-white hover:text-secondary duration-200'>Check Out</button>
-                {/* </Link> */}
-            </Flex>
-
-        </Container>
-            {/* <PortionHeading text="Cart totals" className="text-right mt-5"/> */}
-          </section> 
     </>
   )
 }
