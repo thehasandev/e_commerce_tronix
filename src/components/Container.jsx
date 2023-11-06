33ff753701f442e1b1a79d4df7b8d43b0f109aea
Hasan <thehasandev@gmail.com>
2023-11-06 13:32:10 +0600

    tittle-add

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children,className}) {
   return (
-    <div className={`max-w-container mx-auto ${className}`}>{children}</div>
+    <div className={`max-w-container  mx-auto ${className}`}>{children}</div>
   )
 }
 
