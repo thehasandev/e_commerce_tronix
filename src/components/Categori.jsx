3c6b6e0b56c64142693ed5568c1797a207d8312b
Hasan <thehasandev@gmail.com>
2023-11-06 18:15:17 +0600

    c-size

--- src/components/Categori.jsx
+++ src/components/Categori.jsx
@@ -6,7 +6,7 @@ function Categori({src,alt,text,className}) {
   return (
     <div className={`w-w154 text-center ${className}`}>
         <Image src={src} alt={alt} className="mx-auto  h-[100px]"/>
-        <p className='font-pop font-normal text-2xl text-secondary mt-4'>{text}</p>
+        <p className='font-pop font-normal text-sm lg:text-2xl text-secondary lg:mt-4'>{text}</p>
     </div>
   )
 }
