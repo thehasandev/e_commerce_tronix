dea99a26dc9a30695f3646f9e9988bd34ed72724
Hasan <thehasandev@gmail.com>
2023-11-06 20:07:27 +0600

    footer-middle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,11 +37,11 @@ function Footer() {
            </Container>
        </Section>
 
-       <footer>
+       <footer className='px-2 xl:px-0'>
         <Container>
-            <Flex className="pt-14 pb-6 border-b border-gray">
-                <div className='w-1/2'>
-                    <div className='w-1/2'>
+            <Flex className="pt-14 pb-6 justify-between border-b border-gray flex-col md:flex-row">
+                <div className='md:w-1/2'>
+                    <div className='md:w-1/2'>
                     <Image src={Logo}/> 
                     <p className='font-pop font-normal text-small text-secondary mt-4 mb-8'>Lorem ipsum dolositamet, consectetur adipiscing elit, sed do eiusmod tempor. </p>
                         <Contact icon={<BsFillTelephoneFill size={20} className='mb-4'/>} text="+1234567890"/>
@@ -51,32 +51,32 @@ function Footer() {
                 </div>
 
 
-                <Flex className='w-1/2'>
+                <Flex className='md:w-1/2 justify-between mt-5 xl:mt-0'>
 
-                        <div className='w-1/3'>
-                            <h3 className="font-mon font-bold text-2xl text-secondary mb-8">Company</h3>
+                        <div className='md:w-1/3'>
+                            <h3 className="font-mon font-bold xl:text-2xl text-secondary mb-8">Company</h3>
                             <ul>
-                              <List text="About" className="mb-4"/>
-                              <List text="Products" className="mb-4"/>
-                              <List text="Contact" className="mb-4"/>
-                              <List text="Blog" className="mb-4"/>
-                              <List text="Careers" className="mb-4"/>
+                              <List text="About" className="mb-2  xl:mb-4"/>
+                              <List text="Products" className=" mb-2 xl:mb-4"/>
+                              <List text="Contact" className=" mb-2 xl:mb-4"/>
+                              <List text="Blog" className=" mb-2 xl:mb-4"/>
+                              <List text="Careers" className=" mb-2 xl:mb-4"/>
                             </ul>
                         </div>
 
-                         <div className='w-1/3'>
-                            <h3 className="font-mon font-bold text-2xl text-secondary mb-8">Information</h3>
+                         <div className='md:w-1/3'>
+                            <h3 className="font-mon font-bold xl:text-2xl text-secondary mb-8">Information</h3>
                             <ul>
-                              <List text="Help Center "  className="mb-4"/>
-                              <List text="Payment Methods" className="mb-4"/>
-                              <List text="Return & Refund" className="mb-4"/>
-                              <List text="Privacy Policy" className="mb-4"/>
+                              <List text="Help Center "  className="mb-2  xl:mb-4"/>
+                              <List text="Payment Methods" className="mb-2  xl:mb-4"/>
+                              <List text="Return & Refund" className=" mb-2 xl:mb-4"/>
+                              <List text="Privacy Policy" className=" mb-2 xl:mb-4"/>
                             </ul>
                         </div>
 
-                         <div className='w-1/3'>
-                            <h3 className="font-mon font-bold text-2xl text-secondary mb-8">Follow Us</h3>
-                              <Flex className="gap-x-8">
+                         <div className='md:w-1/3'>
+                            <h3 className="font-mon font-bold xl:text-2xl text-secondary mb-8">Follow Us</h3>
+                              <Flex className="md:gap-x-8 flex-col gap-y-2 xl:flex-row">
                                   <div className='bg-primary w-14 h-14 text-white relative rounded-[8px]'>
                                     <BsInstagram size={30} className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30'/>
                                   </div>
